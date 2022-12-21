c41c6bc59bb26de98fc189e88b3b19a0def2ed80
Ogushanakin <ogzhnakn96@gmail.com>
2022-12-21 08:08:12 +0300

    Project Setup

---
+++ Uber/Uber/Controller/LoginController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginController.swift
+//  Uber
+//
+//  Created by AKIN on 21.12.2022.
+//
+
+import UIKit
+
+final class LoginController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+}
