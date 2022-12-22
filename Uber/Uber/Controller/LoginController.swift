52476c514080c8c65372f21a27bff92ea14df63e
Ogushanakin <ogzhnakn96@gmail.com>
2022-12-22 19:38:25 +0300

    Setting Up Reusable Function For Constraints

--- Uber/Uber/Controller/LoginController.swift
+++ Uber/Uber/Controller/LoginController.swift
@@ -9,9 +9,29 @@ import UIKit
 
 final class LoginController: UIViewController {
     
+    // MARK: - Properties
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "UBER"
+        label.font = UIFont(name: "Avenir-Light", size: 36)
+        label.textColor = UIColor(white: 1, alpha: 0.8)
+        return label
+    }()
+    
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .red
+        view.backgroundColor = UIColor.init(red: 25/225, green: 25/225, blue: 25/225, alpha: 1)
+        
+        view.addSubview(titleLabel)
+        titleLabel.anchor(top: view.safeAreaLayoutGuide.topAnchor)
+        titleLabel.centerX(inView: view)
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
     }
 }
