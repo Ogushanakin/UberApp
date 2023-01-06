551a59b1a12f83f8956c0167222aeed1cb8d93ae
Ogushanakin <ogzhnakn96@gmail.com>
2023-01-06 22:22:27 +0300

    Location Input View UI & Dismissal

--- Uber/Uber/Controller/HomeController.swift
+++ Uber/Uber/Controller/HomeController.swift
@@ -17,6 +17,7 @@ final class HomeController: UIViewController {
     private let locationManager = CLLocationManager()
     
     private let inputActivationView = LocationInputActivationView()
+    private let locationInputView = LocationInputView()
     
     // MARK - Lifecycle
     
@@ -74,6 +75,20 @@ final class HomeController: UIViewController {
         mapView.showsUserLocation = true
         mapView.userTrackingMode = .follow
     }
+    
+    func configureLocationInputView() {
+        locationInputView.delegate = self
+        view.addSubview(locationInputView)
+        locationInputView.anchor(top: view.topAnchor, left: view.leftAnchor,
+                                 right: view.rightAnchor, height: 200)
+        locationInputView.alpha = 0
+        
+        UIView.animate(withDuration: 0.5, animations: {
+            self.locationInputView.alpha = 1
+        }) { _ in
+            print("DEBUG: Present table view..")
+        }
+    }
 }
 
 // MARK: - LocationServices
@@ -108,8 +123,26 @@ extension HomeController: CLLocationManagerDelegate {
     }
 }
 
+// MARK: - LocationInputActivationViewDelegate
+
 extension HomeController: LocationInputActivationViewDelegate {
     func presentLocationInputView() {
-        print("DEBUG: Handle present location input view..")
+        inputActivationView.alpha = 0
+        configureLocationInputView()
     }
 }
+
+// MARK: - LocationInputViewDelegate
+
+extension HomeController: LocationInputViewDelegate {
+    func dismissLocationInputView() {
+        UIView.animate(withDuration: 0.3, animations: {
+            self.locationInputView.alpha = 0
+        }) { _ in
+            UIView.animate(withDuration: 0.3, animations: {
+                self.inputActivationView.alpha = 1
+            })
+        }
+    }
+}
+
